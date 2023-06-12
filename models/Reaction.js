a5d486f3a5a8ecb6ca5b9eab3bd011eaacaf6b3d
Joaquin Salazar <joaqsala@gmail.com>
2023-06-12 12:07:04 -0500

    added reactionSchema

---
+++ models/Reaction.js
@@ -0,0 +1,38 @@
+const { Schema, Types } = require('mongoose');
+
+const reactionSchema = new Schema(
+  {
+    reactionId: {
+      type: Schema.Types.ObjectId,
+      default: () => new Types.ObjectId(),
+    },
+    reactionBody: {
+      type: String,
+      required: true,
+      maxlength: 280,
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: function (timestamp) {
+        const date = new Date(timestamp);
+        const options = { month: 'short', day: 'numeric', year: 'numeric', hour: 'numeric', minute: 'numeric' };
+        const formattedDate = date.toLocaleDateString('en-US', options);
+
+        return `${formattedDate} at ${date.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric' })}`;
+      }
+    },
+  },
+  {
+    toJSON: {
+      getters: true,
+    },
+    id: false,
+  }
+);
+
+module.exports = reactionSchema;
