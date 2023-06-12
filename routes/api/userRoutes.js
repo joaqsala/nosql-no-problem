8836f0e38ae75fd00d1f0b887f1d4fc1247316b3
Joaquin Salazar <joaqsala@gmail.com>
2023-06-12 13:48:17 -0500

    added thoughtsRoutes.js in the api routes folder

--- routes/api/userRoutes.js
+++ routes/api/userRoutes.js
@@ -13,4 +13,5 @@ router.route('/').get(getUsers).post(createUser);
 // /api/users/:userId
 router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser);
 
+
 module.exports = router;
